d9a6f37e3156db73b0132ba58b0484f03a371f96
Charan <58394919+charanpy@users.noreply.github.com>
2021-11-24 10:21:18 +0530

    added url

--- src/component/Profile/ProfileGit.js
+++ src/component/Profile/ProfileGit.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getGithubRepos } from '../../action/profile';
-import spinner from '../layout/spinner';
+
 const ProfileGit = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
     getGithubRepos(username);
